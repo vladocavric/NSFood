f932b1bdf6c24e5f210a168e56e603c25071804d
vladocavric <32539882+vladocavric@users.noreply.github.com>
2020-03-22 21:09:14 +0100

    1st commit

---
+++ app.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const app = express();
+
+app.get('/', function (req, res) {
+    res.send('this is home page for NS Food app')
+});
+app.listen(3000, err => {
+    if(err){
+        console.log(err)
+    } else {
+        console.log('this is working')
+    }
+});
