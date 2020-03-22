6ef1e2e768448c2afc701ef42c000f0c42288a59
vladocavric <32539882+vladocavric@users.noreply.github.com>
2020-03-22 22:39:41 +0100

    change home to send

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ app.set('view engine', 'ejs');
 app.use(express.static('themes'));
 //===================================================================================
 app.get('/', function (req, res) {
-    res.render('home')
+    res.send('something about home page')
 });
 
 //===================================================================================
