eee1d909131bc17adce651d28bc99491fe344a38
vladocavric <32539882+vladocavric@users.noreply.github.com>
2020-03-23 15:09:28 +0100

    env var

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const mongoose = require('mongoose');
 //===================================================================================
-mongoose.connect('mongodb+srv://NSFood:123456+8@nsfood-pifpl.mongodb.net/NSFood?retryWrites=true&w=majority', {useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false}).then(() => {
+mongoose.connect(process.env.NSFOODDB, {useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false}).then(() => {
     console.log('connected to DB');
 }).catch(err => {
     console.log(err);
@@ -11,16 +11,10 @@ mongoose.connect('mongodb+srv://NSFood:123456+8@nsfood-pifpl.mongodb.net/NSFood?
 app.set('view engine', 'ejs');
 app.use(express.static('themes'));
 //===================================================================================
-const PostSchema = new mongoose.Schema({
-    title: String,
-    description: String,
-});
 
-const Post = mongoose.model('Post', PostSchema);
 //===================================================================================
 app.get('/', async (req, res) => {
-    let post = await Post.create({title: 'test', description: 'test description'});
-    res.render('home', {post: post});
+    res.render('home');
 });
 
 app.get('*', function (req, res) {
@@ -35,5 +29,3 @@ app.listen(process.env.PORT, err => {
         console.log('this is working')
     }
 });
-
-//some comment
