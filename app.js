e692af81078cd226a3e57a03219f345e469480b5
vladocavric <32539882+vladocavric@users.noreply.github.com>
2020-03-23 14:16:00 +0100

    added patials and connection to atlas

--- app.js
+++ app.js
@@ -1,11 +1,26 @@
 const express = require('express');
 const app = express();
+const mongoose = require('mongoose');
+//===================================================================================
+mongoose.connect('mongodb+srv://NSFood:123456+8@nsfood-pifpl.mongodb.net/NSFood?retryWrites=true&w=majority', {useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false}).then(() => {
+    console.log('connected to DB');
+}).catch(err => {
+    console.log(err);
+});
 //===================================================================================
 app.set('view engine', 'ejs');
 app.use(express.static('themes'));
 //===================================================================================
-app.get('/', function (req, res) {
-    res.send('home')
+const PostSchema = new mongoose.Schema({
+    title: String,
+    description: String,
+});
+
+const Post = mongoose.model('Post', PostSchema);
+//===================================================================================
+app.get('/', async (req, res) => {
+    let post = await Post.create({title: 'test', description: 'test description'});
+    res.render('home', {post: post});
 });
 
 app.get('*', function (req, res) {
